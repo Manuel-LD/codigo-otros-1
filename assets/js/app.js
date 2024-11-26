95f0b071d9a4e9a62f272e1f5e628b0f8ac3f1ab
Manuel-LD <manuel.lade@hotmail.com>
2024-11-26 16:44:20 -0600

    Cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,20 @@ const $n = document.querySelector('name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //agregué async porque es necesario al usar la funciion await
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json(); //le di uso a la variiable response
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`; //cambio de comillas
+  $b.textContent = `${data.blog}`;//cambio de comillas
+  $l.textContent = `${data.location}`;//cambio de comillas
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` //La variable estaba mal escrita
 }
 
 displayUser('stolinski').catch(handleError);
